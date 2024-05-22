0bb8766aa28673fa6eb7e72ad4ab2d376a4a95a1
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-05-22 12:17:12 +0300

    fetch local API

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+
+  async function fetchData(){
+    const response = await fetch("http://localhost:8001/transactions");
+    const transactions = await response.json();
+    setTransactions(transactions);
+    console.log(transactions);
+
+  }
 
   function addTransaction(transaction) {
     setTransactions([...transactions, transaction]);
@@ -13,6 +21,8 @@ function AccountContainer() {
   function handleSearchChange(event) {
     setSearchTerm(event.target.value);
   }
+
+  useEffect(() => {fetchData()}, [])
 
   return (
     <div>
